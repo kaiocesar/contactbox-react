cb41c259d6b54f14bfebc98fdebb681df2119606
Kaio Cesar <tecnico.kaio@gmail.com>
2019-12-13 07:39:59 -0300

    bundle.js e constante do heroku

--- src/js/components/constants.js
+++ src/js/components/constants.js
@@ -1,4 +1,4 @@
 'use strict'
 
-export const BACKEND_ENDPOINT = 'http://localhost:8000/api/'
+export const BACKEND_ENDPOINT = 'https://gentle-ridge-29291.herokuapp.com/api/' //'http://localhost:8000/api/'
 export const WHATSAPP_URL = 'https://api.whatsapp.com/send?phone='
