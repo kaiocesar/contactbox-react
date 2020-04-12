a7dab5a3b4fd6cfb89bb4fafb28775abcf8796b5
Kaio Cesar <tecnico.kaio@gmail.com>
2020-04-11 23:06:05 -0300

    Update

--- src/js/components/constants.js
+++ src/js/components/constants.js
@@ -1,4 +1,4 @@
 'use strict'
 
-export const BACKEND_ENDPOINT = 'http://127.0.0.1:8000/api/' // 'https://gentle-ridge-29291.herokuapp.com/api/' 
+export const BACKEND_ENDPOINT = 'http://127.0.0.1:80291.herokuapp.com/api/'
 export const WHATSAPP_URL = 'https://api.whatsapp.com/send?phone='
